696d0256f5bb9061b9e1d0a9b4a317b845cd9a2f
ClintonLaptop <clintieleroux@gmail.com>
2022-08-23 17:14:02 +0200

    Initialize Github projects#1
    Move Utilities Project from BitBucket To Github

---
+++ Utilities/Utility.Rest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Text;
+
+namespace Utility
+{
+    public static class Rest
+    {
+        /// <param name="url">string URL to query</param>
+        /// <returns>Json Results</returns>
+        public static async Task<string> Get(string url, Dictionary<string, string> headers)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            foreach (var header in headers)
+                client.DefaultRequestHeaders.Add(header.Key, header.Value);
+
+            return await client.GetStringAsync(url);
+        }
+
+    }
+}
